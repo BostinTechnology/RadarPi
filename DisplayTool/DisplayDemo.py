ab824d1ddcfd45e3db955578d51aa856dfca29e5
Mattb1969 <matthew@bostintechnology.com>
2018-09-15 21:40:48 +0100

    Initial Version

--- DisplayTool/DisplayDemo.py
+++ DisplayTool/DisplayDemo.py
@@ -45,7 +45,7 @@ CANVAS_MARGIN = 10                  # The border around the edge
 CANVAS_WIDTH = (END_X) + (2 * CANVAS_MARGIN)      # Calculated Canvas width
 CANVAS_HEIGHT = (END_Y) + (2 * CANVAS_MARGIN)     # Calculated canvas height
 
-MAX_LENGTH = 500        # The maximum number of values to be displayed on the graph
+MAX_LENGTH = 100        # The maximum number of values to be displayed on the graph
 
 
 class RadarDisplay(Frame):
@@ -53,32 +53,35 @@ class RadarDisplay(Frame):
     Provide teh display part of the application. Data is passed to it from the
     main class below in the form of a queue
     """
-    def __init__(self, master, queue, exit_command, speed_command):
+    def __init__(self, master, queue, exit_command, refresh_command):
         self.log = logging.getLogger()
         self.log.debug("[Disp] cls_RadarDisplay initialised")
         Frame.__init__(self,master)
         self.master = master
         self.queue = queue
         self.exit_command = exit_command
-        self.speed_command = speed_command
+        self.refresh_command = refresh_command
 
         self.log.info("[Disp] Starting Main Frame")
         
         # These are the tuples of what is selected in the listbox
         self.app_dep = StringVar()
         self.refresh_rate = IntVar()
+        self.graph_max = IntVar()
         
         # Removed as scale markers don't make sense with multiple lines
-        #self.y_max_scale_value = IntVar()
-        #self.y_min_scale_value = IntVar()
+        self.y_max_scale_value = IntVar()
+        self.y_min_scale_value = IntVar()
         
         self.running = False       # When true, data is being captured.
         self.refresh_rate.set(SS.DEFAULT_REFRESH_RATE)
         self.dataset = []           # The dataset being displayed and graphed
+        for i in range(0,MAX_LENGTH):           # set all the values to zero to ensure full dataset used.
+            self.dataset.append(0)
+            
         self.app_dep.set(SS.NO_DATA)
-        # Removed as scale markers don't make sense with multiple lines
-        #self.y_max_scale_value.set(0)
-        #self.y_min_scale_value.set(0)
+        self.y_max_scale_value.set(SS.GRAPH_DEFAULT)
+        self.y_min_scale_value.set(0)
 
         # Build the Approaching / Departing frame
         appdep_frame = Frame(self, relief='ridge')
@@ -106,22 +109,33 @@ class RadarDisplay(Frame):
         x_min_label = Label(self.graph_canvas, relief='sunken', text='Oldest')
         x_min_label_window = self.graph_canvas.create_window(START_X, END_Y, anchor=NW, window=x_min_label)
         # Removed as scale markers don't make sense with multiple lines
-        #y_max_label = Label(self.graph_canvas, relief='sunken', textvariable=self.y_max_scale_value, width=5)
-        #y_max_label_window = self.graph_canvas.create_window(START_LEGEND_Y, START_Y, anchor=NW, window=y_max_label)
-        #y_min_label = Label(self.graph_canvas, relief='sunken', textvariable=self.y_min_scale_value, width=5)
-        #y_min_label_window = self.graph_canvas.create_window(START_LEGEND_Y, END_Y-10, anchor=NW, window=y_min_label)
+        y_max_label = Label(self.graph_canvas, relief='sunken', textvariable=str(self.y_max_scale_value), width=5)
+        y_max_label_window = self.graph_canvas.create_window(START_LEGEND_Y, START_Y, anchor=NW, window=y_max_label)
+        y_min_label = Label(self.graph_canvas, relief='sunken', textvariable=str(self.y_min_scale_value), width=5)
+        y_min_label_window = self.graph_canvas.create_window(START_LEGEND_Y, END_Y-10, anchor=NW, window=y_min_label)
         
         self.graph_canvas.pack()
         graph_frame.grid(row=1, column=0)
 
         # Build the faster / slower frame
         fast_slow_frame = Frame(self, relief='ridge')
-        self.speed_units_lbl = Label(fast_slow_frame, relief='sunken', text="in uS", width=10, anchor="center")
+        self.speed_units_lbl = Label(fast_slow_frame, relief='sunken', text="Graph Max", width=10, anchor="center")
         self.speed_units_lbl.grid(row=0, column=0, padx=10)
+        self.speed_units_lbl = Label(fast_slow_frame, relief='sunken', text="in Hz", width=10, anchor="center")
+        self.speed_units_lbl.grid(row=1, column=0, padx=10)
+        self.speed_lbl = Label(fast_slow_frame, relief='sunken', textvariable=str(self.y_max_scale_value), width=10, anchor="center")
+        self.speed_lbl.grid(row=2, column=0, padx=10)
+        self.faster_button = Button(fast_slow_frame, text="INC", command=self.increase_max).grid(row=3, column=0, padx=10)
+        self.slower_button = Button(fast_slow_frame, text="DEC", command=self.decrease_max).grid(row=4, column=0, padx=10)
+
+        self.speed_units_lbl = Label(fast_slow_frame, relief='sunken', text="Refresh Rate", width=10, anchor="center")
+        self.speed_units_lbl.grid(row=5, column=0, padx=10)
+        self.speed_units_lbl = Label(fast_slow_frame, relief='sunken', text="in mS", width=10, anchor="center")
+        self.speed_units_lbl.grid(row=6, column=0, padx=10)
         self.speed_lbl = Label(fast_slow_frame, relief='sunken', textvariable=str(self.refresh_rate), width=10, anchor="center")
-        self.speed_lbl.grid(row=1, column=0, padx=10)
-        self.faster_button = Button(fast_slow_frame, text="INC", command=self.increase_speed).grid(row=2, column=0, padx=10)
-        self.slower_button = Button(fast_slow_frame, text="DEC", command=self.decrease_speed).grid(row=3, column=0, padx=10)
+        self.speed_lbl.grid(row=7, column=0, padx=10)
+        self.faster_button = Button(fast_slow_frame, text="INC", command=self.increase_speed).grid(row=8, column=0, padx=10)
+        self.slower_button = Button(fast_slow_frame, text="DEC", command=self.decrease_speed).grid(row=9, column=0, padx=10)
         fast_slow_frame.grid(row=1, column=1)
 
         # Build the exit frame
@@ -131,7 +145,7 @@ class RadarDisplay(Frame):
 
         # Put it all together on the screen
         self.pack(fill=BOTH, expand=NO)
-        self.after(100, self.main)      # was root.
+        #self.after(100, self.main)      # was root.
         self.log.debug("[Disp] Main Frame Initialised")
         return
 
@@ -155,7 +169,7 @@ class RadarDisplay(Frame):
         self.refresh_rate.set(self.refresh_rate.get() + SS.REFRESH_INCREMENT)
         if self.refresh_rate.get() > SS.MAX_REFRESH_RATE:
             self.refresh_rate.set(SS.MAX_REFRESH_RATE)
-        self.speed_command(self.refresh_rate.get())
+        self.refresh_command(self.refresh_rate.get())
         return
 
     def decrease_speed(self):
@@ -165,17 +179,37 @@ class RadarDisplay(Frame):
         self.refresh_rate.set(self.refresh_rate.get() - SS.REFRESH_INCREMENT)
         if self.refresh_rate.get() < SS.MIN_REFRESH_RATE:
             self.refresh_rate.set(SS.MIN_REFRESH_RATE)
-        self.speed_command(self.refresh_rate.get())
+        self.refresh_command(self.refresh_rate.get())
         return
 
+    def increase_max(self):
+        """
+        Increase the maximum graph value
+        """
+        self.y_max_scale_value.set(self.y_max_scale_value.get() + SS.GRAPH_INC)
+        if self.y_max_scale_value.get() > SS.GRAPH_MAX:
+            self.y_max_scale_value.set(SS.GRAPH_MAX)
+        return
+
+    def decrease_max(self):
+        """
+        Increase the maximum graph value
+        """
+        self.y_max_scale_value.set(self.y_max_scale_value.get() - SS.GRAPH_INC)
+        if self.y_max_scale_value.get() < SS.GRAPH_MIN:
+            self.y_max_scale_value.set(SS.GRAPH_MIN)
+        
+        return
     def reset(self):
         """
         Reset the valus back to the start for another dataset
         """
         self.dataset = []
+        for i in range(0,MAX_LENGTH):           # set all the values to zero to ensure full dataset used.
+            self.dataset.append(0)
         # Removed as scale markers don't make sense with multiple lines
-        #self.y_max_scale_value.set(0)
-        #self.y_min_scale_value.set(0)
+        self.y_max_scale_value.set(SS.GRAPH_DEFAULT)
+        self.y_min_scale_value.set(0)
         self.graph_canvas.delete('graphline')
         #clear the queue of data
 #        self.queue.queue.clear()
@@ -195,12 +229,56 @@ class RadarDisplay(Frame):
             if len(self.graph_canvas.find_withtag('graphline'))> 0:
                 self.graph_canvas.delete('graphline')
         x_spacing = (END_X - START_X) / len(data_to_draw)       # Add 1 as
+
+        # Auto graph scaling
+#turned off
+#        if self.y_max_scale_value.get() < max(data_to_draw):
+#            self.y_max_scale_value.set(max(data_to_draw))
+#        if self.y_min_scale_value.get() > min(data_to_draw):
+#            self.y_min_scale_value.set(min(data_to_draw))
+
+        # Set minimum for Y scale to zero
+        self.y_min_scale_value.set(0)
+
+        if (self.y_max_scale_value.get() - self.y_min_scale_value.get()) > 0:
+            y_spacing = (END_Y - START_Y) / (self.y_max_scale_value.get() - self.y_min_scale_value.get())
+        else:
+            y_spacing = (END_Y - START_Y)
+        self.log.debug("Spacing (x,y):%s,%s" % (x_spacing, y_spacing))
+        self.log.debug("Y Max and min:%s,%s" % (self.y_max_scale_value.get(), self.y_min_scale_value.get()))
+        start_line_x = START_X        # Set the starting point for the X axis
+        start_line_y = END_Y - ((data_to_draw[0] - self.y_min_scale_value.get()) * y_spacing)
+        for reading in data_to_draw[1:]:        # Start at the second reading as first one is starting point
+            end_line_x = start_line_x + x_spacing
+            end_line_y = END_Y - ((reading - self.y_min_scale_value.get()) * y_spacing)
+            #print("END_Y:%s, reading:%s, end_line_y:%s" % (END_Y, reading, end_line_y))
+            self.graph_line = self.graph_canvas.create_line(start_line_x,start_line_y,end_line_x,end_line_y, fill=colours[line_no], tags=('graphline'))
+            start_line_x = end_line_x
+            start_line_y = end_line_y
+        #print(data_to_draw)
+        return
+
+    def draw_graph_no_max(self,data_to_draw, line_no):
+        """
+        Draw the graph on the graph area
+        Graph area is 0 - 20 sections wide, 0 - 13 high
+        coord ranges 20 - END_X across, 20 - 280 up.
+        """
+
+        colours = ["red", "green", "blue", "cyan", "yellow", "magenta", "black", 'white']
+
+        if line_no == 0:
+            # If the line has been drawn before, delete it.
+            if len(self.graph_canvas.find_withtag('graphline'))> 0:
+                self.graph_canvas.delete('graphline')
+        x_spacing = (END_X - START_X) / len(data_to_draw)       # Add 1 as
+        
         y_max = max(data_to_draw)
         y_max = y_max + (y_max * 0.1)       # Add 10% margin to the graph boundary
         y_min = min(data_to_draw)
         y_min = y_min - ( y_min * 0.1)      # Take 10% off the minimum for the graph boundary
 
-        #y_min = 0       # Test to see if stay s stable
+        y_min = 0       # Test to see if stay s stable
 
         if (y_max - y_min) > 0:
             y_spacing = (END_Y - START_Y) / (y_max - y_min)
@@ -219,7 +297,7 @@ class RadarDisplay(Frame):
             start_line_y = end_line_y
         #print(data_to_draw)
         return
-
+        
     def draw_graph_combined(self,data_to_draw, line_no):
         """
         Draw the graph on the graph area
@@ -259,6 +337,7 @@ class RadarDisplay(Frame):
     def exit_program(self):       # Needs to be moved to Data Capture
         # called form the capture button
         logging.debug("Exiting Program")
+        self.running = False
         self.exit_command()
         return
    
@@ -270,11 +349,18 @@ class RadarDisplay(Frame):
         new_reading = 0
         previous_reading = 0
         self.log.info("[Disp] Updating incoming data")
+        freq = 0
         while self.queue.qsize():
             try:
                 data = self.queue.get(0)
                 #self.log.debug("[Disp] Incoming data to be added to feed:%s" % data)
-                self.dataset.append(data)
+                #self.dataset.append(data)
+                freq = (1/(data*2))
+                if freq > SS.GRAPH_MAX:
+                    # The the maximum value is greater than the max allowed, set it to the min allowed as it was a rogue value
+                    self.dataset.append(self.y_min_scale_value.get())
+                else:
+                    self.dataset.append(freq)         # Use frequency instead
             except Queue.Empty:
                 # just on general principles, although we don't
                 # expect this branch to be taken in this case
@@ -285,15 +371,15 @@ class RadarDisplay(Frame):
         if len(self.dataset) > (SS.APP_DEP_MEASURE_QTY*2)+1:
             # If we now have enough data to estimate departing or approaching
             # Compare the last set (defined by APP_DEP_MEASURE_QTY and the previous set of vaules to determine if approaching or departing
-            new_reading = sum(self.dataset[-SS.APP_DEP_MEASURE_QTY:]) / SS.APP_DEP_MEASURE_QTY
-            previous_reading = sum(self.dataset[-SS.APP_DEP_MEASURE_QTY*2:-SS.APP_DEP_MEASURE_QTY]) / SS.APP_DEP_MEASURE_QTY
-            if new_reading > previous_reading:
+            new_reading = int(sum(self.dataset[-SS.APP_DEP_MEASURE_QTY:]) / SS.APP_DEP_MEASURE_QTY)
+            previous_reading = int(sum(self.dataset[-SS.APP_DEP_MEASURE_QTY*2:-SS.APP_DEP_MEASURE_QTY]) / SS.APP_DEP_MEASURE_QTY)
+            if new_reading < previous_reading:
                 self.app_dep.set(SS.DEPARTING)
-            elif previous_reading > new_reading:
+            elif previous_reading < new_reading:
                 self.app_dep.set(SS.APPROACHING)
             else:
                 self.app_dep.set(SS.STABLE)
-            self.log.debug("[Disp] Calculated distance change:%s" % self.app_dep.get())
+            self.log.debug("[Disp] Object is %s (old / mew readings): (%s / %s)" % (self.app_dep.get(), previous_reading, new_reading))
         self.log.info("[Disp] Dataset after update and trim:%s" % self.dataset)
         return
         
@@ -305,7 +391,6 @@ class RadarDisplay(Frame):
         if self.running:
             # Call this when running
             self.update_data()
-            self.log.debug("[Disp] Drawing graph for dataset 0 (FIXED)")
             if len(self.dataset) > 0:
                 self.draw_graph(self.dataset, 0)
         
@@ -325,13 +410,13 @@ class DataCapture:
         self.log.info("[DataC] Starting Class DataCapture")
         self.master = master
         self.capturing = True
-        self.speed = SS.DEFAULT_REFRESH_RATE
+        self.refresh = SS.DEFAULT_REFRESH_RATE
 
         # Create the queue for data transfer
         self.queue = queue.Queue()
 
         # Set up the GUI part
-        self.gui = RadarDisplay(master, self.queue, self.exit_command, self.speed_command)
+        self.gui = RadarDisplay(master, self.queue, self.exit_command, self.refresh_command)
 
         # Set up the thread to do asynchronous I/O
         # More threads can also be created and used, if necessary
@@ -354,7 +439,7 @@ class DataCapture:
         self.log.debug("[DataC] GUI Update cycle starting")
 
         self.gui.main()     # Jog GUI to update stuff
-        self.master.after(SS.GRAPH_REFRESH, self.gui_update)
+        self.master.after(self.refresh, self.gui_update)
         return
 
     def read_gpio(self):
@@ -381,19 +466,21 @@ class DataCapture:
         current_state = False
 
         while self.capturing:
-            time.sleep((self.speed/100000))        # self.speed is given in uS, but time wants it in S
+            time.sleep(SS.CAPTURE_SPEED)
             starttime = self.current_time()
             while True:
                 state = self.read_gpio()
                 nowtime = self.current_time()
                 if state != current_state:
                     # The state has changed since the last cycle
-                    period = (nowtime - starttime)
-                    starttime = nowtime
-                    current_state = state
-                    break
+                    time.sleep(SS.DEBOUNCE_TIME)
+                    if state == self.read_gpio():
+                        period = (nowtime - starttime)
+                        starttime = nowtime
+                        current_state = state
+                        break
             self.queue.put(period)
-            #self.log.debug("[DataC] Random data generated:%s" % msg)
+            #self.log.debug("[DataC] Time Period Read:%s" % period)
         return
 
     def time_period_read_random(self):
@@ -405,7 +492,7 @@ class DataCapture:
         self.log.info("[DataC] Starting Data Capturing thread:%s" % self.capturing)
 
         while self.capturing:
-            time.sleep((self.speed/1000000))        # self.speed is given in uS, but time wants it in S
+            time.sleep(SS.CAPTURE_SPEED)        # self.speed is given in uS, but time wants it in S
             msg = random.randint(0, 100)
             self.queue.put(msg)
             #self.log.debug("[DataC] Random data generated:%s" % msg)
@@ -420,7 +507,7 @@ class DataCapture:
         self.log.info("[DataC] Starting Data Capturing thread:%s" % self.capturing)
 
         while self.capturing:
-            time.sleep((self.speed/1000000))        # self.speed is given in uS, but time wants it in S
+            time.sleep(SS.CAPTURE_SPEED)        # self.speed is given in uS, but time wants it in S
             msg = 0.00000123
             self.queue.put(msg)
             #self.log.debug("[DataC] Random data generated:%s" % msg)
@@ -436,7 +523,7 @@ class DataCapture:
         value = 0
         delta = 1
         while self.capturing:
-            time.sleep((self.speed/1000000))        # self.speed is given in uS, but time wants it in S
+            time.sleep(SS.CAPTURE_SPEED)        # self.speed is given in uS, but time wants it in S
             value = value + delta
             self.queue.put(value)
             if value > 10000:
@@ -454,7 +541,15 @@ class DataCapture:
         self.speed = new_speed
         self.log.debug("[DataC] New Speed value (in uS):%s" % self.speed)
         
-
+        
+    def refresh_command(self,new_refresh_speed):
+        """
+        Take the data given by the GUI and set the speed for data capture
+        The value given is in uS
+        """
+        self.refresh = new_refresh_speed
+        self.log.debug("[DataC] New Speed value (in uS):%s" % self.refresh)
+        
     def exit_command(self):
         """
         Finish the application
--- DisplayTool/SystemSettings.py
+++ DisplayTool/SystemSettings.py
@@ -6,14 +6,21 @@ Contains all the system settings required to be used
 
 # Refresh Rate settings for the data capture, NOT graph
 DEFAULT_REFRESH_RATE = 100
-REFRESH_INCREMENT = 5
+REFRESH_INCREMENT = 25
 MAX_REFRESH_RATE = 100000
 MIN_REFRESH_RATE = 5
 
 GRAPH_REFRESH = 250             # time in mS between screen refreshes
+CAPTURE_SPEED = 0.0005#0.000005        # The time between GPIO readings, 5uS
+DEBOUNCE_TIME = 0.000001
 
 APP_DEP_MEASURE_QTY = 20            # The number of readings to be used for determinging the
                                     # the quantitiy of readings to use to determine approaching or departing
+
+GRAPH_MAX = 2000              # The default maximum value for the graph
+GRAPH_MIN = 10              # The lowest maximum value allowed
+GRAPH_DEFAULT = 500           # The default graph y max value
+GRAPH_INC = 10             # the increment / decrement value for the y max
 
 DEPARTING = "Departing"
 APPROACHING = "Approaching"
