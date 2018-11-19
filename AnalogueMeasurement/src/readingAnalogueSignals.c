e733cffbcb5392cdf9df44b116760432b3f93d81
Mattb1969 <matthew@bostintechnology.com>
2018-11-19 20:25:01 +0000

    Added functionality to draw graphs of voltage being read

--- AnalogueMeasurement/src/readingAnalogueSignals.c
+++ AnalogueMeasurement/src/readingAnalogueSignals.c
@@ -61,6 +61,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <bcm2835.h>        // hardware definition library file
+#include <time.h>           // to enable time functions
 #include "../inc/spi_comms.h"
 
 
@@ -70,6 +71,12 @@
 ******************************************************************************
 */
 
+// Creates a dataset that includes a voltage float value and a time based time value
+struct readings {
+    int     element;
+    float   voltage;
+    time_t  readtime;
+} ;
 
 /*
 ******************************************************************************
@@ -112,62 +119,189 @@ int splashscreen(void)
     return (0);
 }
 
+/*!*********************************************************************
+ *
+ * Setup the plotting functionality
+ *
+ * */
+FILE *prepare_plot() {
+
+//    FILE *lp;
+    
+    printf("Preparing Plot\n");
+
+    FILE *lp = popen("gnuplot -persistent", "w");        //persistent ensures it stays on screen when the program completes
+                //
+
+    fprintf(lp, "set title \"Voltage Levels\"\n");
+    fprintf(lp, "set xlabel \"Time (ticks)\"\n");
+    fprintf(lp, "set ylabel \"Voltage (V)\"\n");
+    fprintf(lp, "set key off\n");
+    fprintf(lp, "set yrange [0:3]\n");
+
+    fprintf(lp, "plot '-' with lines\n");
+        
+    return lp;
+
+}
+
+/*!*********************************************************************
+ *
+ * Add a value to the plot
+ *
+ * */
+void add_data_to_plot(FILE *p, float x, float y, int element) {
+
+    //printf("Adding Data:%d\n", element);
+
+    fprintf(p, "%f %f\n", x, y);                    // Adding data to graph
+
+    return;
+}
+
+/*!*********************************************************************
+ *
+ * Execute the plotting functionality
+ *
+ * */
+void execute_plot(FILE *p) {
+
+    printf("Excuting Plot\n");
+    
+    fprintf(p, "e\n");
+
+    return;
+}
+
+/*!*********************************************************************
+ *
+ * Close the plotting functionality
+ *
+ * */
+void close_plot(FILE *p) {
+
+    printf("Closing Plot\n");
+    
+    pclose(p);
+
+    return;
+}
+
 /*************************************************************************/
-void readValuesContinously(void)
-{
-    printf("DEBUG: Into read Values Continously\n");
-    uint8_t         i;
+float readVoltage(void) {
     uint8_t         msgLen = 3;               // The length of the message 
     uint8_t         txBuf[msgLen];            // The outgoing message
     uint8_t         rxBuf[msgLen];            // The reply from the A-D
-    CommsRetCode   ret;
     float           reading=0.00;                // The output value
-    float         supply_voltage = 3.3;     // SUpply voltage for the A-D
+    float           supply_voltage = 3.3;     // SUpply voltage for the A-D
     uint16_t        fsd_range=4096;           // Maximum value from A-D (12 bits)
+    CommsRetCode   ret;
+    
+    //printf("DEBUG: Into read Voltage\n");
 
-    printf("DEBUG: Defined variables\n");
     txBuf[0] = 0x01; //0b00000001 - 001                  // Start Bit
     txBuf[1] = 0xF0; //0b11110000 - 240                  // Remainder of message to send  
     txBuf[2] = 0x00; //0b00000000 - 000                  // Dummy to get MCP3202 to send return data    
 
+    ret = SPiTranscieve( txBuf, rxBuf, msgLen);
+
+    /* For debug purposes
+    printf("Response:");
+    for (i=0; i < msgLen; i++)
+    {
+        printf("%02x", rxBuf[i]);
+    }
+    printf("\n");
+    */
+
+    /* Shift the bits to the correct locatin
+     * the first byte back is ignored as it only there for comms
+     * the second byte is the MSByte (lower 4 bits anyway, rest are zero)
+     * the third byte is the LSByte
+     * */
+    
+    reading = (rxBuf[1] << 8) + rxBuf[2];
+    //printf("DEBUG: Reading after merge:%f\n", reading);
+
+    // Formula for calculation      Digital Output Code = 4096 * Vin
+    //                                                    ----------
+    //                                                       Vdd
+    reading = reading * (supply_voltage / fsd_range);
+
+    //printf("DEBUG: Voltage being returned:%f\n", reading);
+
+    return reading;
+}
+
+/*************************************************************************/
+void readValuesContinously(void)
+{
+    printf("DEBUG: Into read Values Continously\n");
+    float           voltage;
+
     printf("Reading Values\n");
     do
     {
-        ret = SPiTranscieve( txBuf, rxBuf, msgLen);
-
-        /* For debug purposes
-        printf("Response:");
-        for (i=0; i < msgLen; i++)
-        {
-            printf("%02x", rxBuf[i]);
-        }
-        printf("\n");
-        */
-
-        /* Shift the bits to the correct locatin
-         * the first byte back is ignored as it only there for comms
-         * the second byte is the MSByte (lower 4 bits anyway, rest are zero)
-         * the third byte is the LSByte
-         * */
-        
-        reading = (rxBuf[1] << 8) + rxBuf[2];
-        //printf("DEBUG: Reading after merge:%f\n", reading);
-
-        // Formula for calculation      Digital Output Code = 4096 * Vin
-        //                                                    ----------
-        //                                                       Vdd
-        reading = reading * (supply_voltage / fsd_range);
-
-        printf("Output from A-D:%f\n", reading);
+        voltage = readVoltage();
+        printf("Output from A-D:%f\n", voltage);
     } while (1);
     
     return;
 }
 
-    
 
 
+/*!*********************************************************************
+ *
+ * read values with timestamp for 30 seconds / 2000 readings
+ *
+ * */
+void createDataset(void) {
+    time_t      testendtime, testcurrenttime;
+    int         testduration = 10;
+    float       voltage=0.00;
+    int         max_readings=2000;
+    //float       dataset[max_readings][2];           // dataset is 2000 readings by 2 values (voltage and time)
+    int         i=0;
+    struct readings dataset[max_readings];
+    FILE        *gnu_plot = prepare_plot();         // create instance for plotting
+
+    printf("Creating dataset of values...\n");
+
+    testcurrenttime = clock();                       // capture the test start time
+    testendtime = testcurrenttime + (testduration * CLOCKS_PER_SEC);
+
+    do {
+        
+        testcurrenttime = clock();
+        voltage = readVoltage();
+        dataset[i].element = i;
+        dataset[i].voltage = voltage;
+        dataset[i].readtime = testcurrenttime;
+        //printf("Dataset:%d  Test Time:%ld  Voltage:%f\n", dataset[i].element, dataset[i].readtime, dataset[i].voltage);
+        i++;
+    } while ((testcurrenttime < testendtime) & (i < max_readings));
+
+    for (i=0; i < max_readings; i = i + 50) {
+        printf("Voltage:%f  Time:%ld\n", dataset[i].voltage, dataset[i].readtime);
+    }
+
+    for (i=0; i < max_readings; i++) {
+        add_data_to_plot(gnu_plot, dataset[i].readtime, dataset[i].voltage, i);
+    }
     
+    execute_plot(gnu_plot);
+
+    printf("Plot drawn, press any key to continue");
+
+    getchar();
+
+    close_plot(gnu_plot);
+    
+    return;
+
+} 
+
 
 /******************************************************************************
  *
@@ -186,6 +320,7 @@ int main(int argc, char** argv) {
         printf("**************************************************************************\n");
         printf("Available commands: -\n\n");
         printf("c - Read Voltage Continuously\n");
+        printf("d - Create dataset of Voltage & time for 30 seconds\n");
         printf("e - Exit program \n");
         printf("\n");
 
@@ -202,6 +337,11 @@ int main(int argc, char** argv) {
                 printf("Reading Values...\n");
                 readValuesContinously();
                 break;
+
+            case 'd':
+                /* Create a dataset of voltage and time for 30 seconds */
+                printf("Creating dataset...\n");
+                createDataset();
 
             case 'e':
                 printf("Exiting.......\n");
